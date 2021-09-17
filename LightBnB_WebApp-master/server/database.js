de1c46a1bd645294bcda493157310ed42b57e972
magotts <techybet@gmail.com>
2021-09-16 23:29:21 -0400

    modifying the getAllProperties, taking out the WHERE and just put AND.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -144,27 +144,24 @@ const getAllProperties = function (options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  JOIN property_reviews ON properties.id = property_id 
+  WHERE true
   `;
 
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `AND city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    if (queryParams.length === 1) {
-      queryString += `WHERE owner_id = $${queryParams.length} `;
-    } queryString += `AND owner_id = $${queryParams.length} `;
+    queryString += `AND owner_id = $${queryParams.length} `;
   }
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
-    if (queryParams.length === 2) {
-      queryString += `WHERE cost_per_night BETWEEN  $${queryParams.length - 1} AND $${queryParams.length}`;
-    } queryString += `AND cost_per_night BETWEEN  $${queryParams.length - 1} AND $${queryParams.length}`;
+    queryString += `AND cost_per_night BETWEEN  $${queryParams.length - 1} AND $${queryParams.length}`;
   }
 
   queryString += ` GROUP BY properties.id`;
